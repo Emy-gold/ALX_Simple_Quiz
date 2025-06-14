4c04ba54304762cbcf0e93e35fc8c2979a052ce1
Emy-gold <imanelgarate@gmail.com>
2025-06-14 22:17:57 +0100

    add files

---
+++ quiz.js
@@ -0,0 +1,24 @@
+// Define the checkAnswer function
+function checkAnswer() {
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+
+    // Check if the user selected an answer
+    if (userAnswer) {
+        const userAnswerValue = userAnswer.value;
+
+        if (userAnswerValue === correctAnswer) {
+            document.getElementById("feedback").innerHTML = "Correct! Well done!";
+        } else {
+            document.getElementById("feedback").innerHTML = "That's incorrect. Try again.";
+        }
+    } else {
+        document.getElementById("feedback").innerHTML = "Please select an answer!";
+    }
+
+    // Disable the submit button after the first click
+    document.getElementById("submit-answer").disabled = true;
+}
+
+// Select the "Submit Answer" button and add the click event listener
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
